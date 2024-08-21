1d32cff14cef5ed7bf3d1e62ae453be930b66e53
tugasonline05 <tugasonline05@gmail.com>
2024-08-21 04:55:33 +0000

    AUTH dan Nav Menu

---
+++ lib/app/modules/Bottom_Menu/views/bottom_menu_view.dart
@@ -0,0 +1,38 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../../Biodata/views/biodata_view.dart';
+import '../../Counter/views/counter_view.dart';
+import '../../Profile/views/profile_view.dart';
+import '../../home/views/home_view.dart';
+import '../controllers/bottom_menu_controller.dart';
+
+class BottomMenuView extends GetView<BottomMenuController> {
+  BottomMenuView({Key? key}) : super(key: key);
+
+  final List<Widget> pages = [
+    HomeView(),
+    CounterView(),
+    BiodataView(),
+    ProfileView(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Obx(() => pages[controller.selectedIndex.value]),
+      bottomNavigationBar: ConvexAppBar(
+        items: const [
+          TabItem(icon: Icons.home, title: 'Home'),
+          TabItem(icon: Icons.add, title: 'Counter'),
+          TabItem(icon: Icons.person, title: 'Biodata'),
+          TabItem(icon: Icons.person, title: 'Profile'),
+        ],
+        initialActiveIndex: 0,
+        onTap: controller.changePage,
+      ),
+    );
+  }
+}
